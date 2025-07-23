f6a5d44fbdc26f7902e5c8f855cf5fa2a2e607e8
User Dmitry <dimitry.brosalin@yandex.ru>
2025-07-24 01:36:34 +0300

    WalletApp v.1 delete unused imports

--- src/main/java/ru/wallet/exception/ErrorHandler.java
+++ src/main/java/ru/wallet/exception/ErrorHandler.java
@@ -1,10 +1,7 @@
 package ru.wallet.exception;
 
-import org.hibernate.engine.internal.Collections;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -12,8 +9,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
